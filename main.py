39a6d2115496dbe471b70b395c71fbb4d5660d75
naedavis <naeemahndavis@gmail.com>
2021-06-03 16:30:36 +0200

    Incomplete

---
+++ main.py
@@ -0,0 +1,4 @@
+#Interface 1 will handle login for users.
+#Interface 2 will then allow users to check their status of an excursion depending with the amount they have in the bank account
+# and also handle the exceptions.
+# The program should allow the user to enter the amount that she/he has.
